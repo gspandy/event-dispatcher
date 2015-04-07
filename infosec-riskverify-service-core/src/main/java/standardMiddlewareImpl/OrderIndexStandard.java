ecac3e0757b87208ddf289ae1d1a39c7985e5457
zhengby <zhengby@DST61666.cn1.global.ctrip.com>
2015-04-07 17:21:14 +0800

    使用Base64方式记录下乱码的CMessage消息，待分析用。

--- infosec-riskverify-service-core/src/main/java/standardMiddlewareImpl/OrderIndexStandard.java
+++ infosec-riskverify-service-core/src/main/java/standardMiddlewareImpl/OrderIndexStandard.java
@@ -11,33 +11,43 @@ import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by zhangsx on 2015/2/3.
  */
 public class OrderIndexStandard implements StandardMiddleware {
 
+    private static final Logger logger = LoggerFactory.getLogger(OrderIndexStandard.class);
     @Autowired
     @Qualifier("commonHandler")
     private Handler handler;
 
     public void assembleAndSend(Channel channel, String eventPoint, byte[] body) {
 
-        Map bodyMap = Utils.JSON.parseObject(new String(body, Charset.forName("utf-8")), Map.class);
-        List<Map> subjects = (List<Map>) bodyMap.get("Subjects");
-        for (Map item : subjects) {
-            RiskFact fact = new RiskFact();
-            fact.setEventPoint(eventPoint);
+        String bodyTxt = new String(body, Charset.forName("utf-8"));
+        if (StringUtils.startsWith(bodyTxt, "{") && StringUtils.endsWith(bodyTxt, "}")) {
+            Map bodyMap = Utils.JSON.parseObject(bodyTxt, Map.class);
+            List<Map> subjects = (List<Map>) bodyMap.get("Subjects");
+            for (Map item : subjects) {
+                RiskFact fact = new RiskFact();
+                fact.setEventPoint(eventPoint);
 
-            Map _map = new HashMap();
-            for (Object o : item.keySet()) {
-                String key = o.toString();
-                String first = key.substring(0, 1).toLowerCase();
-                String rest = key.substring(1, key.length());
-                _map.put(first + rest, item.get(key));
+                Map _map = new HashMap();
+                for (Object o : item.keySet()) {
+                    String key = o.toString();
+                    String first = key.substring(0, 1).toLowerCase();
+                    String rest = key.substring(1, key.length());
+                    _map.put(first + rest, item.get(key));
+                }
+                fact.setEventBody(_map);
+                handler.send(channel, fact);
             }
-            fact.setEventBody(_map);
-            handler.send(channel, fact);
+        } else {
+            logger.warn("CMessage[" + eventPoint + "] Base64Body: " + Base64.encodeBase64String(body));
         }
     }
 }
